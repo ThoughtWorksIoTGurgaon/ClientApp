3a1a77c9c01203bf991950c1a2e7e54dae02079f
devilisssh <gauravmanchanda16@gmail.com>
2015-09-01 13:31:35 +0530

    Added Express.js to the app. JS and CSS moved to assets folder. Page functioning properly.

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,6 +1,6 @@
 $(function(){
   console.log("hello!!!!")
-  var socket = io.connect();
+  socket = io.connect();
   socket.on('connect', function () {
     socket.on('message', function(message) {
       console.log("hahaha!!!!")
